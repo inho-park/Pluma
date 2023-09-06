ec7f06431fe30ed5136059c54119f1d8fd968575
JunSeong_Park <ttotto0608@naver.com>
2023-09-06 17:35:50 +0900

    feat : basic setting

---
+++ src/main/java/com/dowon/fluma/FlumaApplication.java
@@ -0,0 +1,13 @@
+package com.dowon.fluma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlumaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlumaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dowon/fluma/FlumaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dowon.fluma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlumaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
