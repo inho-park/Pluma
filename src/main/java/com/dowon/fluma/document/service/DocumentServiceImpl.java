e6d1a3bf817e4edd52682eb58792b82c11e40976
JunSeong_Park <ttotto0608@naver.com>
2023-09-08 14:05:55 +0900

    feat : adding save document DocumentServiceImpl

--- src/main/java/com/dowon/fluma/document/service/DocumentServiceImpl.java
+++ src/main/java/com/dowon/fluma/document/service/DocumentServiceImpl.java
@@ -2,21 +2,32 @@ package com.dowon.fluma.document.service;
 
 import com.dowon.fluma.common.dto.PageResultDTO;
 import com.dowon.fluma.common.dto.StatusDTO;
+import com.dowon.fluma.document.domain.Document;
 import com.dowon.fluma.document.dto.DocumentDTO;
 import com.dowon.fluma.document.dto.DocumentModifyDTO;
 import com.dowon.fluma.document.dto.DocumentPageRequestDTO;
+import com.dowon.fluma.document.repository.DocumentRepository;
+import com.dowon.fluma.user.domain.User;
+import com.dowon.fluma.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Log4j2
 @Service
 @RequiredArgsConstructor
 public class DocumentServiceImpl implements DocumentService {
+    private final UserRepository userRepository;
+    private final DocumentRepository documentRepository;
 
     @Override
     public DocumentDTO saveDocument(DocumentDTO documentDTO) {
-        return null;
+        User user = userRepository.findByName(documentDTO.getName()).orElseThrow();
+        Document document = dtoToEntity(documentDTO, user);
+        documentRepository.save(document);
+        return entityToDTO(document, user);
     }
 
     @Override
