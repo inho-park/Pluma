3a735dfb3d108e60703d8918e5c8527d83e33083
JunSeong_Park <ttotto0608@naver.com>
2023-10-06 00:06:15 +0900

    refactor : adding dto constructor

--- src/main/java/com/dowon/fluma/version/dto/VersionDTO.java
+++ src/main/java/com/dowon/fluma/version/dto/VersionDTO.java
@@ -1,12 +1,16 @@
 package com.dowon.fluma.version.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class VersionDTO {
     private Long versionId;
     private String subtitle;
