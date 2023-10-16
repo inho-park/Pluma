bf0490acc9d0f2b09396cfd7998e33274d343f8d
JunSeong_Park <ttotto0608@naver.com>
2023-10-16 23:34:27 +0900

    refactor : sending email test

--- src/test/java/com/dowon/fluma/Email/EmailSendTests.java
+++ src/test/java/com/dowon/fluma/Email/EmailSendTests.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Random;
+
 @SpringBootTest
 public class EmailSendTests {
     @Autowired
@@ -12,6 +16,18 @@ public class EmailSendTests {
 
     @Test
     public void 이메일_보내기() {
-
+        Random random = null;
+        try {
+            random = SecureRandom.getInstanceStrong();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < 6; i++) {
+            builder.append(random.nextInt(10));
+        }
+        mailService.sendEmail("이메일 입력",
+                "이메일 인증하기 참 쉽죠잉",
+                "인증번호 : " + builder.toString());
     }
 }
